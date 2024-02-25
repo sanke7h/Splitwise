fc11f171cb5e28fd1c77e4c328466f6153b22da5
Vrushank T S <vrushank@vrushank.ts-251111>
2024-02-25 16:26:43 +0530

    group not admin feature added

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const upload = multer({ storage: storage })
 
 const connection =  mysql.createConnection({
     host: 'localhost',
-    password: '12345678',
+    password: 'root',
     user: 'root',
     database: 'splitwise',
   });
@@ -232,6 +232,12 @@ app.post('/group_details',(req,res)=>{
                         return;
                     }
                     const memberIds = memberid.map(member => member.userId);
+                    if(memberIds.length == 0)
+                    {
+                        // const users = [];
+                        const members = [];
+                        return res.render("groupdetails", { group: groupResults[0], user: userResults[0] ,users,members});
+                    }
                     connection.query('SELECT * FROM `user` WHERE `userId` IN (?)',[memberIds], (error, members) => {
                         if (error) {
                             console.error('Error executing SQL query:', error);
@@ -342,5 +348,17 @@ app.post('/decline',(req,res)=>{
           return;
         }
         res.redirect('/profile');
+    });
+})
+
+app.get('/user_group', (req, res) =>{
+    const userId=req.session.user_id;
+    connection.query('SELECT * FROM `memberships` INNER JOIN `grup` ON memberships.groupid = grup.groupid INNER JOIN `user` ON user.userId = grup.adminid WHERE memberships.`userId` = ?', [userId], (error, results, fields) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        console.log(results);
+        res.render("user_group", {results});
     });
 })
